eb852ab588307ecf79bf1635ccecbfedad3ae904
Woybi <eyallva@gmail.com>
2015-12-30 15:58:13 +0100

    Improved output of py2exe.bat

--- py2exe.bat
+++ py2exe.bat
@@ -6,29 +6,35 @@ cd bade
 :loop
 echo.
 echo -------------------------------------
-echo press a key to proceed the building
+echo press a key to proceed the building process or exit by pressing CTRL+C
 pause>nul
 
 if EXIST ..\build (
 echo deleting build content...
 rmdir ..\build /s /q
-echo ...done
 
 echo creating build folder...
 mkdir ..\build
-echo ...done
 ) else (
 echo creating build folder...
 mkdir ..\build
-echo ...done
 )
 
 echo building...
 python setup.py build > ../build/log.txt
-echo ...done
 
-echo you can find the log in build\log.txt
-
+echo moving bade/build/program.exe to actual build dir...
 move build\program.exe ..\build\ >nul
 rmdir build /s /q
+
+echo copying level file to build dir...
+mkdir ..\build\game
+copy game\level.map ..\build\game\level.map >nul
+
+echo copying res folder to build dir...
+mkdir ..\build\res
+copy res ..\build\res >nul
+del ..\build\res\__init__.py
+
+echo DONE - you can find the log in build\log.txt
 goto loop
