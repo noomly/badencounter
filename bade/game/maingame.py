40fa0cd31d8b2b04271e12bbff3af3ad122a835f
Woybi <eyallva@gmail.com>
2016-02-25 16:33:34 +0100

    Add a Credits screen

--- bade/game/maingame.py
+++ bade/game/maingame.py
@@ -23,7 +23,7 @@ class MainGame(object):
     def main_loop(self):
         print("entering main_loop")
 
-        state = "MENU" # MENU, GAME, EXIT
+        state = "MENU" # MENU, GAME, CREDITS, EXIT
 
         while state != "EXIT":
             if state == "MENU":
@@ -32,13 +32,16 @@ class MainGame(object):
             if state == "GAME":
                 state = self.__game()
 
+            if state == "CREDITS":
+                state = self.__credits()
+
         print("exiting main_loop")
 
 
     def __menu(self):
         print("entering __menu")
 
-        menu = Menu(self.graphics, ("play", "exit"))
+        menu = Menu(self.graphics, ("play", "credits", "exit"))
 
         state_to_return = "EXIT"
         goon = True
@@ -59,6 +62,10 @@ class MainGame(object):
                 state_to_return = "GAME"
                 goon = False
 
+            if choice == "credits":
+                state_to_return = "CREDITS"
+                goon = False
+
             if choice == "exit":
                 state_to_return = "EXIT"
                 goon = False
@@ -75,7 +82,7 @@ class MainGame(object):
 
             pygame.display.flip()
 
-            self.clock.tick(60)
+            self.clock.tick(30)
 
         print("exiting __menu")
 
@@ -112,9 +119,7 @@ class MainGame(object):
                         if event.key == pygame.K_RETURN:
                             dial_count += 1
 
-                            try:
-                                levels.chars[pnj_name]["dials"][str(dial_count)]
-                            except Exception as ex:
+                            if dial_count > len(levels.chars[pnj_name]["dials"]):
                                 in_dial = False
                                 pnj_name = "null"
                                 dial_count = 1
@@ -155,7 +160,7 @@ class MainGame(object):
                 my_font = pygame.font.Font("res/ubuntumono-r.ttf", 22)
                 my_string = levels.chars[pnj_name]["dials"][str(dial_count)]
                 my_rect = pygame.Rect((0, 0, c.WINDOW_WIDTH, c.WINDOW_HEIGHT/4))
-                rendered_text = Dialog().render_textrect(my_string, my_font, my_rect, (216, 216, 216), (0, 0, 20, 175), 0)
+                rendered_text = Dialog().render_textrect(my_string, my_font, my_rect, (216, 216, 216), (0, 0, 20, 200), 0)
                 self.screen.blit(rendered_text, (0, c.WINDOW_HEIGHT - rendered_text.get_height()))
 
                 # TODO: Change head if ":" or "*"
@@ -164,11 +169,69 @@ class MainGame(object):
 
                 continue_font = my_font
                 continue_font.set_italic(True)
-                continue_font_rendered = continue_font.render("Press enter to continue...", 1, (150, 150, 150))
+                dial_left = "(" + str(dial_count) + "/" + str(len(levels.chars[pnj_name]["dials"])) + ")"
+                continue_font_rendered = continue_font.render("Press Enter to continue... " + dial_left, 1, (150, 150, 150))
                 self.screen.blit(continue_font_rendered, (c.WINDOW_WIDTH-continue_font_rendered.get_width(), c.WINDOW_HEIGHT - continue_font_rendered.get_height()))
             pygame.display.flip()
 
         print("exiting __game")
+
+        return state_to_return
+
+
+    def __credits(self):
+        print("entering __credits")
+
+        background = pygame.transform.scale(self.graphics["credits_background.jpg"], (c.WINDOW_WIDTH, c.WINDOW_HEIGHT))
+
+        font = pygame.font.Font("res/ubuntumono-r.ttf", 50)
+        text = \
+            ["CREDITS :", \
+             "", \
+             "Programming : Eyal CHOJNOWSKI", \
+             "Drawing : Maréva SEI and Alycia MOLLE"]
+
+        font_continue = pygame.font.Font("res/ubuntumono-r.ttf", 22)
+        rendered_text_continue = font_continue.render("Press Escape or Enter to get back to the menu...", 1, (150, 150, 150))
+
+        state_to_return = "EXIT"
+        goon = True
+        while goon:
+            # events
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    goon = False
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_ESCAPE or event.key == pygame.K_RETURN:
+                        state_to_return = "MENU"
+                        goon = False
+
+            # updates
+
+            # draws
+            finalrender = pygame.Surface((c.WINDOW_WIDTH, c.WINDOW_HEIGHT))
+
+            finalrender.blit(background, (0, 0))
+
+            line_posy = 0
+            for line in text:
+                rendered_text = font.render(line, 1, (200, 150, 255))
+                finalrender.blit(rendered_text, (c.WINDOW_WIDTH/2 - rendered_text.get_width()/2, 280 - rendered_text.get_height() + line_posy))
+                line_posy += font.size(line)[1] + 5
+
+            finalrender.blit(rendered_text_continue, (c.WINDOW_WIDTH/2 - rendered_text_continue.get_width()/2, c.WINDOW_HEIGHT - rendered_text_continue.get_height()-10))
+
+            self.screen.blit(pygame.transform.scale(finalrender,
+                                                    (c.WINDOW_WIDTH,
+                                                     c.WINDOW_HEIGHT)),
+                             (0, 0))
+
+            pygame.display.flip()
+
+            self.clock.tick(30)
+
+        print("exiting __credits")
 
         return state_to_return
 
